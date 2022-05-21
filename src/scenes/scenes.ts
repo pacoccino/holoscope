ae518b9f15f44de167b28f503f9e2c5bcf785e06
paco <pakokrew@gmail.com>
2022-05-21 16:17:07 +0200

    local video folder

--- src/scenes/scenes.ts
+++ src/scenes/scenes.ts
@@ -5,10 +5,10 @@ const ScenesParams = {
   video: [VideoScene, window.location.search.replace('?n=', '')],
   orb: [VideoScene, 'orb.mp4'],
   woodpecker: [ThreeScene, 'woodpecker/scene.gltf'],
-  // plume: [VideoScene, 'plume.mp4'],
-  // pac: [VideoScene, 'pac.mp4'],
-  // mitch: [VideoScene, 'mitch.mp4'],
-  // ml: [VideoScene, 'ml.mp4'],
+  // plume: [VideoScene, 'local/plume.mp4'],
+  // pac: [VideoScene, 'local/pac.mp4'],
+  // mitch: [VideoScene, 'local/mitch.mp4'],
+  // ml: [VideoScene, 'local/ml.mp4'],
 }
 const defaultSceneParams = ScenesParams[Object.keys(ScenesParams)[0]]
 
